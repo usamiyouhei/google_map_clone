84db8bfd4b1edd222bb17aa00a70058baa287f18
youhei usami <usao9110@icloud.com>
2026-03-28 12:18:37 +0400

    3/28

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
       <div>Hello</div>
     </>
   );
