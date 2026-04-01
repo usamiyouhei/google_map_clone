00a7f0b8a560e2caca9da72008b18d8ba28ec412
youhei usami <usao9110@icloud.com>
2026-04-01 12:34:53 +0400

    4/1

--- src/components/MapView/index.tsx
+++ src/components/MapView/index.tsx
@@ -1,10 +1,21 @@
-import './index.css';
-import LocateButton from '../LocateButton';
-import RoutePanel from '../RoutePanel';
+import "./index.css";
+import LocateButton from "../LocateButton";
+import RoutePanel from "../RoutePanel";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 export default function MapView() {
   return (
-    <div className='map-wrapper'>
+    <div className="map-wrapper">
+      <MapContainer
+        center={[35.6812, 139.7671]}
+        zoom={15}
+        className="map-container"
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://www.openstreetmap.org/copyright">openstreetmap</a> contributors'
+        />
+      </MapContainer>
       {/* マップはreact-leafletを使用するため、ライブラリ削除後は表示されません */}
       <LocateButton />
       <RoutePanel />
