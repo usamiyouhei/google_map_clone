5d7b7a14ceb2fd1ec1d233b15906456b1001c1d9
youhei usami <usao9110@icloud.com>
2026-04-12 09:38:12 +0400

    4/12

---
+++ src/modules/routes/route.repository.ts
@@ -0,0 +1,19 @@
+import type { LatLng, RouteData } from "./route.state";
+import api from "../../lib/api";
+
+export const routeRepository = {
+  async getRoute(origin: LatLng, destination: LatLng): Promise<RouteData> {
+    const result = await api.get(
+      `http://router.project-osrm.org/route/v1/driving/${origin[1]},${origin[0]};${destination[1]},${destination[0]}?overview=full&geometries=geojson`,
+    );
+    const data = result.data;
+    if (!data.routes || data.routes.length === 0)
+      throw new Error("No route found");
+    const route = data.routes[0];
+    return {
+      coordinates: route.geometry.coordinates,
+      distance: route.distance,
+      duration: route.duration,
+    };
+  },
+};
